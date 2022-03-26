75eee0f5a51254f66eee43e4a2d2d51451f3608c
Gwen Woodruff <mwoodruff1@live.maryville.edu>
2022-03-25 21:17:53 -0500

    Initial Commit

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonList, IonLabel, IonListHeader } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -7,16 +7,33 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Groceries</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Groceries</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+      <IonList>
+      <IonListHeader>
+        <IonLabel>Item</IonLabel>
+        <IonLabel>Amount</IonLabel>
+      </IonListHeader>
+      <IonItem>
+        <IonLabel>Bannana</IonLabel>
+        <IonLabel>3</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Bread</IonLabel>
+        <IonLabel>2</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Scrunchies</IonLabel>
+        <IonLabel>4</IonLabel>
+      </IonItem>
+    </IonList>
       </IonContent>
     </IonPage>
   );
